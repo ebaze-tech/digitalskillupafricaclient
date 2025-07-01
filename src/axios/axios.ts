cd9d633bfc7027afe049f1e3f4398bfe631739aa
ebaze-tech <ebaze.technologies@gmail.com>
2025-07-01 13:57:42 +0100

    Second Push

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 // src/api/axios.ts
 import axios from "axios";
 
-const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
 const API = axios.create({
   baseURL: API_BASE_URL,
